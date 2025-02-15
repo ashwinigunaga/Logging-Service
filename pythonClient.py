faac3e72b76495e767a0bd6187351830938087d7
Jyot Chirag Shah <Jshah1717@conestogac.on.ca>
2025-02-15 11:44:29 -0500

    Added input loop for sending log messages

--- pythonClient.py
+++ pythonClient.py
@@ -5,6 +5,13 @@ def send_log_message(host, port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
     print("Connected to logging server.")
+    
+while True:
+    message = input("Enter log message: ")
+    if message.lower() == 'exit':
+        print("Closing connection.")
+        break
+    client_socket.sendall(message.encode('utf-8'))
 
 if __name__ == "__main__":
     host = sys.argv[1]
