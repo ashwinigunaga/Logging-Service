b4d5309c140a727422eac1dd3f2b3a05d9a3988b
Jyot Chirag Shah <Jshah1717@conestogac.on.ca>
2025-02-16 16:58:08 -0500

    fixed some things

--- pythonClient.py
+++ pythonClient.py
@@ -11,14 +11,14 @@ def send_log_message(host, port):
     formatted_message = formatted_message.replace("{message}", message)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    client_socket.connect((host, port))
-    client_socket.sendall(formatted_message.encode('utf-8'))
-    print("Formatted log message sent successfully.")
+        client_socket.connect((host, port))
+        client_socket.sendall(formatted_message.encode('utf-8'))
+        print("Formatted log message sent successfully.")
     
 if __name__ == "__main__":
     host = sys.argv[1]
     port = int(sys.argv[2])
     message = sys.argv[4]
     log_format = sys.argv[3]
-    
-    send_log_message(host, port)
+
+    send_log_message(host, port, log_format, message)
