062e213a4a9a98ffbe260812b96adb8abc076799
ashwinigunaga <gunagaashwini@gmail.com>
2025-02-16 13:36:34 -0500

    Parsing command line arguments

--- loggingService.java
+++ loggingService.java
@@ -4,3 +4,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+public static void main(String[] args) {
+    if (args.length < 2) {
+        System.out.println("Usage: java LoggingService <port> <logFilePath> <logFormat>");
+        return;
+    }
+}
