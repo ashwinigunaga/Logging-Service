62a323a4e9c4bd046d8224084d82efd1d7948325
Jyot Chirag Shah <Jshah1717@conestogac.on.ca>
2025-02-16 16:45:14 -0500

    Updated log format and fixed some things

--- pythonClient.py
+++ pythonClient.py
@@ -6,21 +6,19 @@ def send_log_message(host, port):
     client_ip = socket.gethostbyname(socket.gethostname())
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+    formatted_message = log_format.replace("{timestamp}", timestamp)
+    formatted_message = formatted_message.replace("{client}", client_ip)
+    formatted_message = formatted_message.replace("{message}", message)
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     client_socket.connect((host, port))
-    print("Connected to logging server.")
-
-while True:
-    message = input("Enter log message: ")
-    if message.lower() == 'exit':
-        print("Closing connection.")
-        break
-
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    formatted_message = f"[{timestamp}] {client_ip}: {message}"
     client_socket.sendall(formatted_message.encode('utf-8'))
+    print("Formatted log message sent successfully.")
     
 if __name__ == "__main__":
     host = sys.argv[1]
     port = int(sys.argv[2])
+    message = sys.argv[4]
+    log_format = sys.argv[3]
+    
     send_log_message(host, port)
